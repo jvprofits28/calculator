bbabceebfbeeaf36060f269ba823c17ce33de1ed
Juan Vazquez <ehulises28@hotmail.com>
2021-09-20 14:11:05 -0700

    added basic structure and some css as well

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import "./App.css";
 
 const App = () => {
+  const [result, setResult] = useState("Hello World");
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <>
+      <div className="container">
+        <form>
+          <input type="text" value={result} />
+        </form>
+        <div className="keypad">
+          <button>clear</button>
+          <button>C</button>
+          <button>&divide;</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>&times;</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>&ndash;</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
+    </>
   );
 };
 
